6e99f134d5cb1423bdef155923b7d1b75b860ac4
Aymeric Agon-Rambosson <aymeric.agon@hotmail.fr>
2019-10-28 16:43:12 +0100

    Ajout début TME5 (vide)

---
+++ TME5/src/algorithms/DefaultTeam.java
@@ -0,0 +1,63 @@
+package algorithms;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+/***************************************************************
+ * TME 1: calcul de diamètre et de cercle couvrant minimum.    *
+ *   - Trouver deux points les plus éloignés d'un ensemble de  *
+ *     points donné en entrée.                                 *
+ *   - Couvrir l'ensemble de poitns donné en entrée par un     *
+ *     cercle de rayon minimum.                                *
+ *                                                             *
+ * class Circle:                                               *
+ *   - Circle(Point c, int r) constructs a new circle          *
+ *     centered at c with radius r.                            *
+ *   - Point getCenter() returns the center point.             *
+ *   - int getRadius() returns the circle radius.              *
+ *                                                             *
+ * class Line:                                                 *
+ *   - Line(Point p, Point q) constructs a new line            *
+ *     starting at p ending at q.                              *
+ *   - Point getP() returns one of the two end points.         *
+ *   - Point getQ() returns the other end point.               *
+ ***************************************************************/
+import supportGUI.Circle;
+import supportGUI.Line;
+
+public class DefaultTeam {
+
+  // calculDiametre: ArrayList<Point> --> Line
+  //   renvoie une paire de points de la liste, de distance maximum.
+  public Line calculDiametre(ArrayList<Point> points) {
+    if (points.size()<3) {
+      return null;
+    }
+
+    Point p=points.get(0);
+    Point q=points.get(1);
+
+    /*******************
+     * PARTIE A ECRIRE *
+     *******************/
+
+    return new Line(p,q);
+  }
+
+  // calculCercleMin: ArrayList<Point> --> Circle
+  //   renvoie un cercle couvrant tout point de la liste, de rayon minimum.
+  public Circle calculCercleMin(ArrayList<Point> points) {
+    if (points.isEmpty()) {
+      return null;
+    }
+
+    Point center=points.get(0);
+    int radius=100;
+
+    /*******************
+     * PARTIE A ECRIRE *
+     *******************/
+
+    return new Circle(center,radius);
+  }
+}
